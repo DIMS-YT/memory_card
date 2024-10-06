5e9a4cc26b7c2e409669af649aabd45368752013
Dima <Baran@sand.com>
2024-10-06 13:31:42 +0300

    06.10 2024

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
+from random import choice, shuffle
 from PyQt6.QtWidgets import QApplication
-
-
-
 app = QApplication([])
 
 from main_window import *
 
+class Question():
+    def __init__(self, question_text, answer_text, wrong:tuple) -> None:
+        self.question_text = question_text
+        self.answer_text = answer_text
+        self.wrong_answers = wrong
+
+    def got_right(self):
+        ...
+    def got_wrong(self):
+        ...
+
+
+q1 = Question("Яблуко", "apple", ("application", "ape", "pineapple"))
+q2 = Question("Фортнайт", "fortnite", ("fornite", "fronti", "firtneto"))
+q3 = Question("Меч", "sword", ("sord", "swodr", "hoorse"))
+q4 = Question("Асасин", "Assassins", ("ashansin", "adolfsin", "ananasin"))
+
+
+questions_list = [q1,q2,q3,q4 ]
+radio_button_list = [rb_answer_1, rb_answer_2, rb_answer_3, rb_answer_4]
+
+def new_question():
+    random_question = choice(questions_list)
+
+    shuffle(radio_button_list)
+
+    question_lb.setText(random_question.question_text)
+
+    radio_button_list[0].setText(random_question.answer_text)
+
+    for i in range(3):
+         radio_button_list[i+1].setText(random_question.wrong_answers[i])
+
+new_question()
 
 def change_box():
     if btn_next.text() == "Відповісти":
@@ -16,6 +48,7 @@ def change_box():
         radio_button_box.show()
         answer_box.hide()
         btn_next.setText("Відповісти")
+        new_question()
 
 
 
